4ba295d6464d6f9f12bafe5e3cd6e059d6be6491
Aleia Knight <aleiaknight@gmail.com>
2019-09-10 15:42:17 -0700

    Fixed bugs. Added more test functions/error handling. More functions added

--- checklist.py
+++ checklist.py
@@ -2,6 +2,7 @@ class colors:
     purple = '\033[30m'
     green = '\033[32m'
     yellow = '\033[93m'
+    grey = '\033[37m'
 
 checklist = list()
 
@@ -11,6 +12,7 @@ def create(item):
 
 # READ
 def read(index):
+    print(checklist[index])
     return checklist[index]
 
 # UPDATE
@@ -29,27 +31,50 @@ def list_all_items():
         index += 1
 
 # MARK COMPLETED
-#def mark_completed(index):
-#    print ('√' + checklist[index])
+def mark_completed(index):
+    update(index, ' √ ' + checklist[index])
 
 # SELECT
 def select(function_code):
     # Create item
-    if function_code == "C":
+    if function_code == "C" or function_code == "c":
         input_item = user_input("Input item: ")
         create(input_item)
 
     # Read item
-    elif function_code == "R":
+    elif function_code == "R" or function_code == "r":
         item_index = int(user_input("Index Number? "))
         # Remember that item_index must actually exist or our program will crash.
         read(item_index)
 
+    # Update
+    elif function_code == "U" or function_code == "u":
+        list_all_items()
+        update_index = input('What list item (by assosciated index) would you like to update? : ')
+        new_item = input("Input new value: ")
+        update(int(update_index), new_item)
+        print('\nHere is the updated list: \n')
+        list_all_items()
+
+    #Destroy
+    elif function_code == "D" or function_code == "d":
+        list_all_items()
+        destroy_index = input('What list item (by assosciated index) would you like to destroy? : ')
+        destroy(int(destroy_index))
+        list_all_items()
+
     # Print all items
-    elif function_code == "P":
+    elif function_code == "P" or function_code == "p":
+        list_all_items()
+
+    elif function_code == "M" or function_code == "m":
+        completed_index = input("Which item would you like to mark completed (enter index): ")
+        mark_completed(int(completed_index))
+        print("Updated List:\n")
         list_all_items()
     
-    elif function_code == "Q":
+    #Quit
+    elif function_code == "Q" or function_code == "q":
         #Where the loop stops
         return False
 
@@ -77,25 +102,41 @@ def test():
     destroy(1)
 
     print(colors.yellow, read(0))
-    #print(read(1))
 
+    print("\nCreate Tests: \n")
+    select("C") #create test
     select("C")
+    select("C") 
+    print("\nRead Test: \n")
+    select("R") #read test
+    print("\nUpdate Test: \n")
+    select("U") #update test
+    print("\nDestroy Test: \n")
+    select("D") #destroy test
+    print("\nPrint All Test: \n")
+    select("P") #print all test
+    print("\nMark Complete Test\n")
+    select("M")
+    print("\nQuit Test: \n")
+    select("Q") #quit test
 
-    list_all_items()
 
-    select("R")
-
-    list_all_items()
-
-    user_value = user_input("Please Enter a value:")
-    print(user_value)
-
-
-test()
+def run():
+    running = True
+    while running:
+        selection = user_input(
+            "\nPress C to create list, R to read, U to update item, D to destroy, M to mark completed, P to print list, and Q to quit  ")
+        running = select(selection)
 
 
-running = True
-while running:
-    selection = user_input(
-        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
-    running = select(selection)
+print(colors.yellow, "Welcome to the Captian Rainbow Checklist!")
+
+start_option = input("Would you like to run the TEST or the full APPLICATION? Enter t/a:  ")
+if start_option == "t" or start_option == 'T':
+    print(colors.green)
+    test()
+elif start_option == 'a' or start_option == "A":
+    print(colors.grey)
+    run()
+else:
+    print("\nInvalid response\nExiting software\n")
